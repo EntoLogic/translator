596a9226822b9d72c7ddd2d580b11ae6d3cc8527
Daniel Mulcahy <ZetaetaDaniel@gmail.com>
2013-11-20 21:47:02 +0000

    Add Test.hs

---
+++ Test.hs
@@ -0,0 +1,12 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Entologic.Ast
+import Entologic.JsonAst
+import Entologic.JsonPhrase
+import Entologic.Translate
+
+main = do
+    phrases <- readPhrases "phrase.json"
+    ast <- readAst "example.json"
+    runTL (TLInfo phrases "ruby" "en") TLState (translate . (!!0) . pEntries $ uProg ast)
