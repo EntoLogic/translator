0eca50dcdf339b8d5d382287e1f2f9e24b867c58
Daniel Mulcahy <ZetaetaDaniel@gmail.com>
2013-12-04 20:56:00 +0000

    Add output format

---
+++ Entologic/Output.hs
@@ -0,0 +1,25 @@
+
+{-# LANGUAGE TemplateHaskell #-}
+
+module Entologic.Output where
+
+import Data.Text
+import Control.Lens.TH
+
+data OutputNode = OutputNode { _oNode :: Text
+                             , _oClauses :: [OutputClause]
+                             , _loc :: Area }
+
+data OutputClause = OCString Text
+                  | OCNode OutputNode
+                  | OCAnnot Annotation
+
+data Annotation = NoAnnotation
+
+data Area = Area { _start :: Maybe Location, _end :: Maybe Location }
+
+data Location = Location { _line :: Int, _col :: Int }
+
+$(makeLenses ''OutputNode)
+$(makeLenses ''Area)
+$(makeLenses ''Location)
