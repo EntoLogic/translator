c006a31250bfee7ab31fa3e4dab2887e5ead301b
Daniel Mulcahy <ZetaetaDaniel@gmail.com>
2013-12-21 14:36:17 +0000

    Add json conversion for output format

--- Entologic/JsonOutput.hs
+++ Entologic/JsonOutput.hs
@@ -1,5 +1,32 @@
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Entologic.JsonOutput where
 
 import Entologic.Output
+import Data.Aeson
+import Control.Applicative
+import Data.Maybe
 
+instance ToJSON OutputNode where
+    toJSON (OutputNode node clauses indent loc) =
+        object [ "node" .= node, "clauses" .= clauses
+               , "indent" .= indent, "location" .= loc]
+
+instance ToJSON OutputClause where
+    toJSON (OCString s) = toJSON s
+    toJSON (OCNode n) = toJSON n
+    toJSON (OCNodes ns) = toJSON ns
+    toJSON (OCAnnot a) = toJSON a
+
+instance ToJSON Annotation where
+    toJSON _ = object []
+
+instance ToJSON Area where
+    toJSON (Area start end) =
+        object . catMaybes $ [pair "start" start, pair "end" end]
+      where
+        pair name val = (name .=) . toJSON <$> val
+
+instance ToJSON Location where
+    toJSON (Location line col) = toJSON [line, col]
--- Test.hs
+++ Test.hs
@@ -5,11 +5,16 @@ import Entologic.Ast
 import Entologic.JsonAst
 import Entologic.JsonPhrase
 import Entologic.Translate
+import Entologic.JsonOutput
 import Text.Show.Pretty
+import Data.Aeson
+import Data.ByteString.Lazy.Char8 as L
 
 main = do
     phrases <- readPhrases "phrase.json"
     ast <- readAst "example.json"
     x <- runTL (TLInfo phrases "ruby" "en") (TLState False) (translate . (!!0) . pEntries $ uProg ast)
-    putStrLn $ ppShow x
+    case x of
+        (Right y) -> L.putStrLn . encode $ y
+        _ -> return ()
 
